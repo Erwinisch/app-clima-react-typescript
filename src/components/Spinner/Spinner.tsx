af5ff69b19b9af411c6e48a566f344fc0e831ca5
Erwinisch <erwinchavezasenjo@gmail.com>
2024-04-25 18:37:51 -0400

    Primer commit, app clima

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,14 @@
+import './Spinner.css';
+
+function spinner (){
+
+    return (
+        <div className="spinner">    
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+        </div>
+    )
+}
+
+export default spinner;
